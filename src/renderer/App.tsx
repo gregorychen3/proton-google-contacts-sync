ab76a7f98ccce5e35ca67cd8aea6c375b94723ca
Gregory Chen <gregorychen3@gmail.com>
2024-05-21 20:44:35 -0400

    mui hello world

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -2,11 +2,16 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import Button from '@mui/material/Button';
 import { Route, MemoryRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 
 function Hello() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Button variant="contained">Hello world</Button>
+    </div>
+  );
 }
 
 export default function App() {
